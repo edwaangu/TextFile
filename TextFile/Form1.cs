c3e371bd1a48ae511025759c2081c446bca36ed0
edwaangu <edwaangu171@outlook.com>
2021-12-13 09:40:34 -0500

    I have finished

--- TextFile/Form1.cs
+++ TextFile/Form1.cs
@@ -22,28 +22,36 @@ namespace TextFile
 
         private void loadColoursButton_Click(object sender, EventArgs e)
         {
-
+            colours = File.ReadAllLines("Colors.txt").ToList();
             DisplayColours();
         }
 
         private void saveColoursButton_Click(object sender, EventArgs e)
         {
-
+            File.WriteAllLines("Colors.txt", colours);
         }
 
         private void sortColoursButton_Click(object sender, EventArgs e)
         {
-
+            colours = colours.OrderBy(x => x).ToList();
+            DisplayColours();
         }
 
         private void addColourButton_Click(object sender, EventArgs e)
         {
-
+            colours.Add(colourInput.Text);
+            colourInput.Text = "";
+            DisplayColours();
         }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
-
+            string toRemove = removeInput.Text;
+            if (colours.Contains(toRemove))
+            {
+                colours.Remove(toRemove);
+            }
+            DisplayColours();
         }
 
         public void DisplayColours()
@@ -65,28 +73,53 @@ namespace TextFile
 
         private void loadScoresButton_Click(object sender, EventArgs e)
         {
+            List<string> tempString = File.ReadAllLines("scoreFile.txt").ToList();
+            for(int i = 0;i < tempString.Count;i+=2)
+            {
+                string _name = tempString[i];
+                int _score = Convert.ToInt16(tempString[i + 1]);
+
+                scores.Add(new HighScore(_name, _score));
+            }
 
             DisplayScores();
         }
 
         private void saveScoresButton_Click(object sender, EventArgs e)
         {
+            List<string> tempString = new List<string>();
+            for(int i = 0;i < scores.Count;i++)
+            {
+                tempString.Add(scores[i].name);
+                tempString.Add(scores[i].score.ToString());
+            }
 
+            File.WriteAllLines("scoreFile.txt", tempString);
         }
 
         private void sortScoresButton_Click(object sender, EventArgs e)
         {
-
+            scores = scores.OrderByDescending(x => x.score).ToList();
+            DisplayScores();
         }
 
         private void addScoreButton_Click(object sender, EventArgs e)
         {
-
+            scores.Add(new HighScore(nameInput.Text, Convert.ToInt16(scoreInput.Text)));
+            nameInput.Text = "";
+            scoreInput.Text = "";
+            DisplayScores();
         }
 
         private void removeScoresButton_Click(object sender, EventArgs e)
         {
-
+            string toRemove = nameRemove.Text;
+            int indexOf = scores.FindIndex(x => x.name == toRemove);
+            if(indexOf >= 0)
+            {
+                 scores.RemoveAt(indexOf);
+            }
+            DisplayScores();
         }
 
         public void DisplayScores()
